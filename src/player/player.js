eb92643556134349d406949bd9e4cfd5cbc42acf
Maksim Stojkovic <18454392+maksimstojkovic@users.noreply.github.com>
2023-10-28 01:04:41 +1100

    Implement player and AI

--- src/player/player.js
+++ src/player/player.js
@@ -1,11 +1,45 @@
 import { Gameboard } from "../gameboard/gameboard";
 
+const boardSize = 10;
+
 const Player = () => {
-  const gameboard = Gameboard();
+  // Use 10x10 board
+  const gameboard = Gameboard(boardSize, boardSize);
 
-  const attackPlayer = () => {};
+  const getBoard = () => gameboard;
+  const attackPlayer = (player, coord) => {
+    if (coord.length !== 2) return false;
 
-  return { attackPlayer };
+    player.getBoard().receiveAttack(coord);
+    return player.getBoard().isSpaceHit(coord);
+  };
+
+  return { attackPlayer, getBoard };
 };
 
-export { Player };
+const AI = () => {
+  const player = Player();
+  let lastAttackCoordinate = null;
+
+  const getLastAttackCoordinate = () => lastAttackCoordinate;
+
+  const attackPlayer = (player) => {
+    for (;;) {
+      const x = Math.floor(Math.random() * boardSize);
+      const y = Math.floor(Math.random() * boardSize);
+
+      player.getBoard().receiveAttack([x, y]);
+
+      // Loop until confirmed hit
+      if (!player.getBoard().isSpaceHit([x, y])) continue;
+
+      lastAttackCoordinate = [x, y];
+
+      return true;
+    }
+  };
+
+  return { getLastAttackCoordinate, attackPlayer, getBoard: player.getBoard };
+};
+
+export { Player, AI };
